ac9d61c55a912da9a4603cb4606ed5fed02c1b16
bebiksior <bebiks.priv@gmail.com>
2025-09-02 02:06:03 +0200

    cleanup

--- src/types.ts
+++ src/types.ts
@@ -12,14 +12,3 @@ export type HttpMethod = StringWithAutocomplete<
 >;
 
 export type HTTPRequestLines = string[];
-export type LineEnding = "\r\n" | "\n";
-
-export type ModificationResult =
-  | {
-      success: true;
-      result: string;
-    }
-  | {
-      success: false;
-      error: string;
-    };
